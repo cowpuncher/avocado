32142c816d8b63b0007270d102b205d5634209d7
WebDev TVlean <tvleanjohnny@gmail.com>
2022-12-22 19:10:50 +0300

    Добавил стили и функции переноса элементов

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,23 +16,79 @@ window.addEventListener('DOMContentLoaded', e => {
             }
         })
     } 
-    // Функция обрезания текста в уведомлениях
-    const textSlice = (node, number) => {
-        node.forEach(text => {
-            if(text.innerHTML.length > number) {
-                console.log(text.innerHTML.length);
-                text.innerHTML = text.innerHTML.substr(0, number - 1) + ' ... <a href="#" class="notif_list_block_body__more">смотреть еще</a>'
+    // Функция обрезания текста в уведомлениях с кнпкой "смотреть еще"
+    const textSlice = (node, number) => {        
+        node.forEach((text) => {
+            let fullText = text.innerHTML;
+            if(fullText.length > number) {
+                text.innerHTML = text.innerText.substr(0, number - 1) + ' ... ';
+                text.insertAdjacentHTML('afterend', '<a class="notif_list_block_body__more">смотреть еще</a>');
             } 
+            let moreBtn = text.closest('.notif_list_block_body').querySelector('.notif_list_block_body__more');            
+            moreBtn.addEventListener('click', e => {
+                e.target.closest('.notif_list_block_body').classList.toggle('open');
+                if(e.target.closest('.notif_list_block_body').classList.contains('open')) {
+                    text.innerHTML = fullText;
+                    e.target.innerHTML = 'cвернуть';
+                } else {
+                    text.innerHTML = text.innerHTML.substr(0, number - 1) + ' ... ';
+                    e.target.innerHTML = 'смотреть еще';
+                }
+            });
         })
     }
     let notificationText = document.querySelectorAll('.notif_list_block_body__text');
     textSlice(notificationText, 230);
     //Всплывающие блоки рядом с кнопками.
-    let wrapPopup = document.querySelectorAll('.wrap_popupblock');
-    wrapPopup.forEach(function(item) {
-        let openButton = item.querySelector('span');
-        openButton.addEventListener('click', e => toggleActiveClass(e.currentTarget));
-        closeOpenBlock(openButton,  openButton.nextElementSibling);
-    });
- 
+    function popupActivated() {
+        let wrapPopup = document.querySelectorAll('.wrap_popupblock');
+        wrapPopup.forEach(function(item) {
+            let openButton = item.querySelector('span');
+            openButton.addEventListener('click', e => toggleActiveClass(e.currentTarget));
+            closeOpenBlock(openButton,  openButton.nextElementSibling);
+        });
+    } 
+    popupActivated();
+    // Активация стилизации селектов
+    customSelect("select", 1);
+
+    // Ширина прокрутки страницы и ширины экрана
+    const sizeWindow = (screen) => {
+        let scrollBar = window.innerWidth - screen;
+        return screen + scrollBar;
+    }
+    //---------- Клонирование элементов
+    // Клонирование в конец списка
+    const cloneElementWithRemove = (divGet, divInsert) => divInsert.append(divGet);
+    // Клонирование в начало списка
+    const cloneElementBeforeWithRemove = (divGet, divInsert) =>  divInsert.prepend(divGet);
+    // Клонирование в конец списка без удаления элемента
+    const cloneElementWithoutRemove = (divGet, divInsert) => {
+        const newDiv = divGet.cloneNode( true );
+        divInsert.appendChild( newDiv );
+    }
+    // Перенос элементов в зависимости от разрешения
+    function permutationElements() {
+        // Уведомления
+        let notif = document.getElementById('notif');
+        if(sizeWindow(document.body.clientWidth) < 993) { 
+            cloneElementBeforeWithRemove(notif, document.querySelector('.header_top_nav'));
+        } else {
+            cloneElementBeforeWithRemove(notif, document.querySelector('.body_top_nav'));
+        }
+    }
+    permutationElements();
+    // Проверка окна при ресайзе
+    window.addEventListener('resize', e => {
+        permutationElements();
+    })
+   
+
+    let menu = document.querySelector('.menu');
+    let menuWidth = 0;
+    menu.querySelectorAll('li a').forEach(item => {
+        menuWidth = menuWidth + item.clientWidth;
+        console.log( item.scrollWidth);
+    })
+    menu.setAttribute('style', 'width: ' + menuWidth +'px;')
 })
