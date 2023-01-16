50b5e350550a118b8c46d580a5795339112d62b2
WebDev TVlean <tvleanjohnny@gmail.com>
2023-01-16 18:23:58 +0300

    add active elements

--- landing/assets/js/script.js
+++ landing/assets/js/script.js
@@ -1,10 +1,26 @@
 const header = document.querySelector('.header');
+const body = document.querySelector('body');
+const productsWrap = document.querySelector('.products_wrap');
+const productsItem = document.querySelectorAll('.products_item');
+
 
 document.addEventListener('scroll', e => {
+
     if((header.offsetHeight/2) < window.pageYOffset) {
-        header.setAttribute('style','background: #FFEDE8;');
+        body.setAttribute('style','background: #FFEDE8;');
     } else {
-        header.setAttribute('style','background: #ffffff;');
+        body.setAttribute('style','background: #ffffff;');
     }
-    
-})
+
+    productsItem.forEach((item, index) => {
+        if((item.getBoundingClientRect().top + document.documentElement.clientHeight/2) <= 0) {
+            if(index == 0) {
+                body.setAttribute('style','background: #D3F5FF;');
+            } else if(index == 1) {
+                body.setAttribute('style','background: #E9E1FF;');
+            } else if(index == 2) {
+                body.setAttribute('style','background: #FEFFD3;');
+            }
+        }
+    });
+});
